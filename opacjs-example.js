987f1ee6e5a8c2b724fe4a1d8e606939ae356373
phette23 <phette23@gmail.com>
2024-04-09 09:00:39 -0700

    more complete OPAC JS example for public JSON report

--- opacjs-example.js
+++ opacjs-example.js
@@ -1,6 +1,14 @@
-// in reality you'd want an `if` condition to only run on bib detail pages
-// get biblionumber from URL
-let id = new URLSearchParams(location.search).get("biblionumber")
-$.get(`https://koha.library.org/cgi-bin/koha/svc/report?id=42&sql_params=${id}`, (data) => {
-    if (data.length) $('.record').append(`<span class="results_summary"><span class="label">OCLC Holdings: </span> ${data[0][0]}</span>`)
-})
+// run only on bib detail pages
+if (window.location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
+    // accompanies a report with this ID that returns the MARC 948 field for a given bib
+    const reportId = 42
+    // get biblionumber from URL
+    const bib = new URLSearchParams(location.search).get("biblionumber")
+    $.get(`https://koha.library.org/cgi-bin/koha/svc/report?id=${reportId}&sql_params=${bib}`, (data) => {
+        // reports JSON is an array of arrays where child arrays are the query result rows
+        if (data.length) {
+            const html = `<span class="results_summary"><span class="label">OCLC Holdings: </span> ${data[ 0 ][ 0 ]}</span>`
+            $('.record').append(html)
+        }
+    })
+}
